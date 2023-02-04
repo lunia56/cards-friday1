2767c9a58bfa56c7116ad737b99aeefa00057aca
Ilya Lunin <lunia56@mail.ru>
2023-02-04 07:08:12 +0300

    test

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import './App.css';
-import {HashRouter} from 'react-router-dom';
-import {store} from '../m2-bll/store';
-import {Provider} from 'react-redux';
 import Pages from './routes/Pages';
 import Navbar from './routes/Navbar';
 
 function App() {
     return (
         <div className="App">
-            <HashRouter>
-                <Provider store={store}>
-                    <>
-                        <Navbar/>
-                        <Pages/>
-                    </>
-                </Provider>
-            </HashRouter>
+            <>
+                <Navbar/>
+                <Pages/>
+            </>
         </div>
     );
 }
